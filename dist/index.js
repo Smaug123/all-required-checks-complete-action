4a63e004dbe279e5f80e4eb08094a0f1475bc4ea
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2024-08-13 17:08:52 +0100

    Fix

--- dist/index.js
+++ dist/index.js
@@ -16,25 +16,27 @@ function processJob(jobName, jobData) {
     }
 }
 function main() {
-    const resultsJson = process.env.ALL_REQUIRED_CHECKS_COMPLETE_RESULTS || '{}';
+    const resultsJson = process.env["INPUT_NEEDS-CONTEXT"];
+    if (!resultsJson) {
+        throw Error("No needs-context was provided");
+    }
+    ;
     let results;
     try {
         results = JSON.parse(resultsJson);
     }
     catch (error) {
-        console.error(`Error: Unable to parse ALL_REQUIRED_CHECKS_COMPLETE_RESULTS as JSON: ${error}`);
-        return 1;
+        throw (Error(`Error: Unable to parse needs-context as JSON: ${error}`));
     }
     let exitStatus = 0;
     for (const [jobName, jobData] of Object.entries(results)) {
         if (typeof jobData !== 'object' || jobData === null) {
-            console.error(`Unexpected shape at key ${jobName}, expected an object`);
-            return 2;
+            throw (Error(`Unexpected shape at key ${jobName}, expected an object`));
         }
         exitStatus += processJob(jobName, jobData);
     }
     if (exitStatus > 0) {
-        return 3;
+        throw (Error("Some jobs failed!"));
     }
     return 0;
 }
--- src/index.ts
+++ src/index.ts
@@ -20,27 +20,26 @@ function processJob(jobName: string, jobData: JobData): number {
 }
 
 function main(): number {
-    const resultsJson = process.env.ALL_REQUIRED_CHECKS_COMPLETE_RESULTS || '{}';
+    const resultsJson = process.env["INPUT_NEEDS-CONTEXT"];
+    if (!resultsJson) { throw Error("No needs-context was provided") };
     let results: { [key: string]: JobData };
     try {
         results = JSON.parse(resultsJson);
     } catch (error) {
-        console.error(`Error: Unable to parse ALL_REQUIRED_CHECKS_COMPLETE_RESULTS as JSON: ${error}`);
-        return 1;
+        throw(Error(`Error: Unable to parse needs-context as JSON: ${error}`));
     }
 
     let exitStatus = 0;
 
     for (const [jobName, jobData] of Object.entries(results)) {
         if (typeof jobData !== 'object' || jobData === null) {
-            console.error(`Unexpected shape at key ${jobName}, expected an object`);
-            return 2;
+            throw(Error(`Unexpected shape at key ${jobName}, expected an object`));
         }
         exitStatus += processJob(jobName, jobData);
     }
 
     if (exitStatus > 0) {
-        return 3;
+        throw(Error("Some jobs failed!"));
     }
 
     return 0;
